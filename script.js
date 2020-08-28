7fad2466e6b24e4b38682f761a727069b28db12d
Brendon Conatser <b.conatser21@gmail.com>
2020-08-28 10:28:31 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,46 @@
+var today = $('#currentDay');
+var inputHour = moment().format('H');
+
+setInterval(function() {
+    var rightNow = moment();
+    var date =rightNow.format('MMMM Do YYYY, h:mm:ss a');
+    today.text(date);
+}, 100);
+
+$(".saveBtn").click(function () { 
+    $('input[type="text"]').each(function () { 
+        var saveId = $(this).attr('id'); 
+        var saveValue = $(this).val(); 
+        localStorage.setItem(saveId, saveValue);
+    });
+});
+
+$('input[type="text"]').each(function () { 
+    var pullId = $(this).attr('id'); 
+    var pullStored = localStorage.getItem(pullId); 
+    document.getElementById(pullId).value = pullStored; 
+});
+
+function whatTime (time, id) {
+    if (inputHour > time) {
+        $(id).addClass('past');
+    }else if (inputHour < time){
+        $(id).addClass('future');
+    } else {
+        $(id).addClass('present');
+    }
+}
+
+whatTime(8, '#eight');
+whatTime(9, '#nine');
+whatTime(10, '#ten');
+whatTime(11, '#eleven');
+whatTime(12, '#twelve');
+whatTime(13, '#thirteen');
+whatTime(14, '#fourteen');
+whatTime(15, '#fifteen');
+whatTime(16, '#sixteen');
+whatTime(17, '#seventeen');
+whatTime(18, '#eighteen');
+
+
